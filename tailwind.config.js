5ecc3eb4b9d9c6c7fdc14e532e84fe4662dd097e
daveinthahood <davidesimone96@outlook.it>
2024-05-28 18:53:24 +0200

    init: screens and colors setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,21 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens:{
+
+      'phone': '400px',
+      // => @media (min-width: 400px) { ... }
+
+      'tablet': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'laptop': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'desktop': '1280px',
+      // => @media (min-width: 1280px) { ... }
+      
+    },
     extend: {},
   },
   plugins: [],
