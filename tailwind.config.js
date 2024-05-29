7327e984ee5b7a9d644bd5d4ec943a30bac754e0
daveinthahood <davidesimone96@outlook.it>
2024-05-29 08:46:47 +0200

    ADD: process file

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+
+    colors:{
+      transparent: 'transparent',
+      'white': '#ffffff',
+      'purple': '#3f3cbb',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#ecebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+    },
+    
+    
     screens:{
 
       'phone': '400px',
@@ -18,8 +32,14 @@ export default {
 
       'desktop': '1280px',
       // => @media (min-width: 1280px) { ... }
-      
+
     },
+    
+    fontFamily:{
+
+    },
+
+
     extend: {},
   },
   plugins: [],
